b56c9e14316be15747c66b9cd544967240d9d155
Faisal-alshehri0 <445050265@STD.PSAU.EDU.SA>
2026-05-02 01:12:33 +0300

    protecting process Count and waiting taime

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -62,13 +62,23 @@ class SharedResources {
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+        lock.lock();
+        try {
+            completedProcessCount++;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        lock.lock();
+        try {
+            totalWaitingTime += time;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // Method to log execution
